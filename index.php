1a39270634990fc05bb0f40072954cc30eca82c8
IamShirouu <soylakitainu@gmail.com>
2025-01-15 13:49:55 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
-header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *'); // con esta weada, todos los origenes pueden pedir info
+header('content-type: application/json; charset=utf-8'); // el formato en el que devolves
 
-if ($_SERVER['REQUEST_METHOD'] === "GET") {
+$primeraRes = rand(1, 3);
+
+if ($primeraRes === 1) {
+    $primeraRes = "Amate tal y como eres!";
+};
+
+if ($primeraRes === 2) {
+    $primeraRes = "La vida sigue, no te rindas!";
+};
+
+if ($primeraRes === 3) {
+    $primeraRes = "Esta API te quiere mucho";
+};
+
+if ($_SERVER['REQUEST_METHOD'] === "GET") { // nomas get
     $respuesta = [
         "mensaje" => "Amate tal y como eres!",
-        "status" => "exitoso",
+        "status" => $primeraRes,
         "fecha" => date("Y-m-d H:i:s")
     ];
     
     echo json_encode($respuesta);
 }
-
 
 ?>
